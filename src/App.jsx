525210243eb8124228cd1f6ab9d4ec8b95e28894
charleshergott <150687443+charleshergott@users.noreply.github.com>
2024-02-17 20:26:13 +0100

    Day_1 all files

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import { Example } from "./components/mainMenu.jsx"
+import './App.css'
+
+
+function App() {
+  const [count, setCount] = useState(0); // Initialize state using the useState hook
+
+  // Here you can add more logic for your App component
+
+  return (
+    <div className="App">
+      {/* Render the Example component */}
+      <Example />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/assets/App.jsx", "./src/assets/mainMenu.jsx", "./src/assets/main.jsx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
