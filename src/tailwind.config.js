f8a5c301bcc5ac7e33d2f8325309cc2607625a60
charleshergott <150687443+charleshergott@users.noreply.github.com>
2024-04-27 07:39:24 +0200

    commit_270424_initial

--- src/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/assets/App.jsx", "./src/assets/components/mainMenu.jsx", "./src/assets/main.jsx", "./src/assets/components/companyInfo.jsx"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
